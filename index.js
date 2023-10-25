097bfd14035bc541769d12ed2606b7a4a5a1e875
Maciej <mtomaszewski006@gmail.com>
2023-10-25 20:31:21 +0200

    Add db.js file with functions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const {clientRouter} = require('./routers/client');
+const {homeRouter} = require('./routers/home');
+const {db} = require('./utils/db');
+
 
 const app = express();
 
@@ -15,7 +18,9 @@ app.engine('.hbs', hbs.engine({
 }));
 app.set('view engine', '.hbs');
 
+app.use('/', homeRouter);
 app.use('/client', clientRouter);
+
 
 app.listen(3000, 'localhost', () => {
     console.log('Listening on: http://localhost:3000');
