6583b7d94d325d7705ad4b0575cb794fcbeb3c93
Maciej <mtomaszewski006@gmail.com>
2023-10-26 19:39:22 +0200

    Create list-all.hbs with client information

--- routers/client.js
+++ routers/client.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const {db} = require('../utils/db');
 
 const clientRouter = express.Router();
 
 clientRouter
     .get('/', (req,res) => {
-        res.send('Działa');
+        res.render('client/list-all', {
+            clients: db.getAll(),
+        });
+    })
+    .get('/:id', (req,res) => {
+        res.send('Pobierz pojedynczego');
+    })
+    .post('/', (req,res) => {
+        res.send('Dodaj');
+    })
+    .put('/:id', (req,res) => {
+        res.send('Zmodyfikuj');
+    })
+    .put('/:id', (req,res) => {
+        res.send('Usuń');
     });
 
 module.exports ={
